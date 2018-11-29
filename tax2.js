50b61591851be7bbe4698d8842b10e58563500fe
Richmond Wong <richmondwong@gmail.com>
2018-11-29 00:44:57 +0000

    First commit

---
+++ tax2.js
@@ -0,0 +1,87 @@
+
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+function calculateSalesTax(salesData, taxRates) {
+  var aggregator = {}
+
+  for (i in salesData){
+    var name = salesData[i]["name"];
+    var province = salesData[i]["province"];
+
+    var totalRevenue = calculateTotalSalesAmount(salesData[i]["sales"]);
+
+    var taxRate = taxRates[province];
+
+
+    if (aggregator[name]){
+      console.log("Duplicate Entry Detected")
+        aggregator[name]["totalSales"] += totalRevenue;
+        aggregator[name]["totalTaxes"] += calculateTotalTaxAmount(totalRevenue, taxRate)
+
+    } else {
+      aggregator[name] = {
+        "totalSales": totalRevenue,
+        "totalTaxes": calculateTotalTaxAmount(totalRevenue, taxRate)
+      }
+    }
+
+  }
+  return aggregator
+}
+
+
+function calculateTotalSalesAmount(sales){
+  salesAmount = 0
+  for (i in sales){
+    salesAmount += sales[i]
+  }
+  return salesAmount
+}
+
+
+function calculateTotalTaxAmount(totalRevenue, taxRate){
+  taxAmount = totalRevenue * taxRate
+  return taxAmount
+}
+
+
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results)
+// console.log(calculateTotalSalesAmount([ 80, 20, 10, 100, 90, 500 ]))
+console.log(calculateTotalTaxAmount(800, 0.05))
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
